18128572cc232995d17afddba9dd7a55b21d435d
Marabii <minehamza97@gmail.com>
2025-12-28 10:31:00 +0100

    Backend correctly parses log entries when given a matching regex pattern

--- RustBackend/src/main.rs
+++ RustBackend/src/main.rs
@@ -1,20 +1,28 @@
 use std::{
     io::{self, BufRead},
-    rc::Rc,
+    sync::{
+        Arc, Mutex,
+        atomic::{AtomicBool, Ordering},
+    },
+    thread::{self, JoinHandle},
+    time::Duration,
 };
 mod services;
 mod types;
+use regex::Regex;
 use services::file_processor::FileProcessor;
 
 use crate::{
-    services::commands_processor::CommandsProcessor,
+    services::{FileState, commands_processor::CommandsProcessor},
     types::{Command, Response},
 };
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let reader = stdin.lock();
-    let mut processor: Rc<Option<FileProcessor>> = Rc::new(None);
+    let mut file_state: Arc<Mutex<Option<FileState>>> = Arc::new(Mutex::new(None));
+    let mut watcher_handle: Option<JoinHandle<()>> = None;
+    let should_stop = Arc::new(AtomicBool::new(false));
 
     for line in reader.lines() {
         let input_str = line?;
@@ -31,26 +39,74 @@ fn main() -> io::Result<()> {
         };
 
         match command {
-            Command::OpenFile { path } => {
-                processor = Rc::new(Some(FileProcessor::new(path).unwrap_or_else(|err| {
-                    eprintln!("Something went wrong: {}", err);
-                    std::process::exit(1);
+            Command::OpenFile {
+                path,
+                pattern,
+                nbr_columns,
+            } => {
+                if let Some(handle) = watcher_handle.take() {
+                    should_stop.store(true, Ordering::Relaxed); //Hey thread, stop what you're doing.
+                    handle.join().expect("Thread panicked"); //i'm waiting for you...
+                    should_stop.store(false, Ordering::Relaxed); //reset the stop signal for another use.
+                }
+
+                let regex_pattern: Option<Regex> =
+                    pattern.and_then(|re_str| Regex::new(&re_str).ok());
+
+                file_state = Arc::new(Mutex::new(Some(FileState {
+                    processor: FileProcessor::new(path).unwrap_or_else(|err| {
+                        eprintln!("Something went wrong: {}", err);
+                        std::process::exit(1);
+                    }),
+                    regex_pattern,
+                    nbr_columns,
                 })));
 
-                if let Some(ref fp) = *processor {
-                    let line_count = fp.index.len() as u64;
+                let cloned_file_state = Arc::clone(&file_state);
+                let stop_flag = Arc::clone(&should_stop);
+                watcher_handle = Some(thread::spawn(move || {
+                    loop {
+                        thread::sleep(Duration::from_secs(1));
+
+                        if stop_flag.load(Ordering::Relaxed) {
+                            break; // Exit the loop
+                        }
+
+                        let mut file_state_guard = cloned_file_state.lock().unwrap();
+                        if let Some(ref mut fp) = *file_state_guard
+                            && let Ok(changed) = fp.processor.refresh_if_needed()
+                            && changed
+                        {
+                            eprintln!("File updated: re-indexed");
+                        }
+                    }
+                }));
+
+                let cloned_file_state = Arc::clone(&file_state);
+                let guard = match cloned_file_state.lock() {
+                    Ok(g) => g,
+                    Err(_poisoned) => {
+                        eprintln!("Something went wrong");
+                        std::process::exit(1);
+                    }
+                };
+
+                if let Some(fs) = guard.as_ref() {
+                    let line_count = fs.processor.index.len() as u64;
                     let data = Response::FileOpened { line_count };
                     let json = serde_json::to_string(&data).unwrap();
                     println!("{}", json);
                 }
             }
             _ => {
-                let response =
-                    CommandsProcessor::process_command(command, processor.as_ref().as_ref());
+                let response = CommandsProcessor::process_command(command, Arc::clone(&file_state));
                 println!("{}", serde_json::to_string(&response).unwrap());
             }
         }
     }
-
+    if let Some(handle) = watcher_handle.take() {
+        should_stop.store(true, Ordering::Relaxed);
+        handle.join().expect("Thread panicked");
+    }
     Ok(())
 }
